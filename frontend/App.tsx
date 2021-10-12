8b91ce9c90320c436f1dae958dbddd1c0d969e95
Shun Sato <shunsato19951231@gmail.com>
2021-10-12 22:48:25 +0900

    <Frontend> mod: providers

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -2,20 +2,17 @@ import React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import { AuthProvider } from './hooks/Auth/Auth.context';
 import { theme } from './theme/default';
-import { combineComponents } from './utils/combineComponents';
-
 import { CssBaseline } from '@mui/material';
 import { Login } from './pages/Login';
 
-const providers = [AuthProvider, () => <ThemeProvider theme={theme} />];
-const AppContextProvider = combineComponents(...providers);
-
 const App: React.FC = () => {
   return (
-    <AppContextProvider>
-      <CssBaseline />
-      <Login />
-    </AppContextProvider>
+    <AuthProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Login />
+      </ThemeProvider>
+    </AuthProvider>
   );
 };
 
